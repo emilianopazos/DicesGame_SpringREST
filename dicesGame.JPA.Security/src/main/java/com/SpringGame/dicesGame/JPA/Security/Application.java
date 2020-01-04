0dc2ef107b41755ebcdffd278464aba4d6ea71ab
Emiliano <yomandrake@gmail.com>
2020-01-04 22:50:31 +0100

    New proyect from Spring Initializer with Spring Security Dependency included

---
+++ dicesGame.JPA.Security/src/main/java/com/SpringGame/dicesGame/JPA/Security/Application.java
@@ -0,0 +1,13 @@
+package com.SpringGame.dicesGame.JPA.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
